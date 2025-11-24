d776ceccfbe270b824ca8bcff85b5b05aea21115
justforit-help <soundousnidar@gmail.com>
2025-11-24 22:43:14 +0100

    Mise à jour compileSdk vers 36

--- androiddevops/app/build.gradle.kts
+++ androiddevops/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.android_devops"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.android_devops"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
